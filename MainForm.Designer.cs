01964f486c511a7bd3d77ef497a36bf28ae4059a
Angelo T. Arboleda <ghelobytes@yahoo.com>
2014-10-18 21:04:52 +0800

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace Briefcase
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.kryptonPanel = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
+            this.kryptonNavigator1 = new ComponentFactory.Krypton.Navigator.KryptonNavigator();
+            this.tabPull = new ComponentFactory.Krypton.Navigator.KryptonPage();
+            this.kryptonPanel1 = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
+            this.lnkDataDir = new ComponentFactory.Krypton.Toolkit.KryptonLinkLabel();
+            this.kryptonLabel1 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.kryptonLabel2 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.btnDownload = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.cmbODKDrive = new ComponentFactory.Krypton.Toolkit.KryptonListBox();
+            this.cmbTabletID = new ComponentFactory.Krypton.Toolkit.KryptonComboBox();
+            this.tabExport = new ComponentFactory.Krypton.Navigator.KryptonPage();
+            this.kryptonPanel2 = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
+            this.optControl = new ComponentFactory.Krypton.Toolkit.KryptonRadioButton();
+            this.optNonControl = new ComponentFactory.Krypton.Toolkit.KryptonRadioButton();
+            this.btnExportExcel = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel)).BeginInit();
+            this.kryptonPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonNavigator1)).BeginInit();
+            this.kryptonNavigator1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tabPull)).BeginInit();
+            this.tabPull.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).BeginInit();
+            this.kryptonPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.cmbTabletID)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabExport)).BeginInit();
+            this.tabExport.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel2)).BeginInit();
+            this.kryptonPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // kryptonPanel
+            // 
+            this.kryptonPanel.Controls.Add(this.kryptonNavigator1);
+            this.kryptonPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonPanel.Location = new System.Drawing.Point(0, 0);
+            this.kryptonPanel.Name = "kryptonPanel";
+            this.kryptonPanel.Padding = new System.Windows.Forms.Padding(20);
+            this.kryptonPanel.Size = new System.Drawing.Size(358, 281);
+            this.kryptonPanel.TabIndex = 0;
+            // 
+            // kryptonNavigator1
+            // 
+            this.kryptonNavigator1.Bar.TabBorderStyle = ComponentFactory.Krypton.Toolkit.TabBorderStyle.RoundedOutsizeLarge;
+            this.kryptonNavigator1.Bar.TabStyle = ComponentFactory.Krypton.Toolkit.TabStyle.StandardProfile;
+            this.kryptonNavigator1.Button.ButtonDisplayLogic = ComponentFactory.Krypton.Navigator.ButtonDisplayLogic.None;
+            this.kryptonNavigator1.Button.CloseButtonDisplay = ComponentFactory.Krypton.Navigator.ButtonDisplay.Hide;
+            this.kryptonNavigator1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonNavigator1.Location = new System.Drawing.Point(20, 20);
+            this.kryptonNavigator1.Name = "kryptonNavigator1";
+            this.kryptonNavigator1.Pages.AddRange(new ComponentFactory.Krypton.Navigator.KryptonPage[] {
+            this.tabPull,
+            this.tabExport});
+            this.kryptonNavigator1.SelectedIndex = 0;
+            this.kryptonNavigator1.Size = new System.Drawing.Size(318, 241);
+            this.kryptonNavigator1.TabIndex = 7;
+            this.kryptonNavigator1.Text = "kryptonNavigator1";
+            // 
+            // tabPull
+            // 
+            this.tabPull.AutoHiddenSlideSize = new System.Drawing.Size(200, 200);
+            this.tabPull.Controls.Add(this.kryptonPanel1);
+            this.tabPull.Flags = 65534;
+            this.tabPull.LastVisibleSet = true;
+            this.tabPull.MinimumSize = new System.Drawing.Size(50, 50);
+            this.tabPull.Name = "tabPull";
+            this.tabPull.Size = new System.Drawing.Size(316, 214);
+            this.tabPull.Text = "Download";
+            this.tabPull.ToolTipTitle = "Page ToolTip";
+            this.tabPull.UniqueName = "FB3974C810E3473408AF9858CB2C18E3";
+            // 
+            // kryptonPanel1
+            // 
+            this.kryptonPanel1.Controls.Add(this.lnkDataDir);
+            this.kryptonPanel1.Controls.Add(this.kryptonLabel1);
+            this.kryptonPanel1.Controls.Add(this.kryptonLabel2);
+            this.kryptonPanel1.Controls.Add(this.btnDownload);
+            this.kryptonPanel1.Controls.Add(this.cmbODKDrive);
+            this.kryptonPanel1.Controls.Add(this.cmbTabletID);
+            this.kryptonPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonPanel1.Location = new System.Drawing.Point(0, 0);
+            this.kryptonPanel1.Name = "kryptonPanel1";
+            this.kryptonPanel1.Size = new System.Drawing.Size(316, 214);
+            this.kryptonPanel1.TabIndex = 0;
+            // 
+            // lnkDataDir
+            // 
+            this.lnkDataDir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lnkDataDir.Location = new System.Drawing.Point(163, 191);
+            this.lnkDataDir.Name = "lnkDataDir";
+            this.lnkDataDir.Size = new System.Drawing.Size(150, 20);
+            this.lnkDataDir.TabIndex = 7;
+            this.lnkDataDir.Values.Text = "Show download directory";
+            this.lnkDataDir.LinkClicked += new System.EventHandler(this.lnkDataDir_LinkClicked);
+            // 
+            // kryptonLabel1
+            // 
+            this.kryptonLabel1.Location = new System.Drawing.Point(35, 27);
+            this.kryptonLabel1.Name = "kryptonLabel1";
+            this.kryptonLabel1.Size = new System.Drawing.Size(60, 20);
+            this.kryptonLabel1.TabIndex = 3;
+            this.kryptonLabel1.Values.Text = "Tablet ID";
+            // 
+            // kryptonLabel2
+            // 
+            this.kryptonLabel2.Location = new System.Drawing.Point(36, 58);
+            this.kryptonLabel2.Name = "kryptonLabel2";
+            this.kryptonLabel2.Size = new System.Drawing.Size(78, 20);
+            this.kryptonLabel2.TabIndex = 5;
+            this.kryptonLabel2.Values.Text = "Source drive";
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(133, 123);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(93, 29);
+            this.btnDownload.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.btnDownload.StateCommon.Border.Rounding = 5;
+            this.btnDownload.TabIndex = 0;
+            this.btnDownload.Values.Text = "Download";
+            this.btnDownload.Click += new System.EventHandler(this.btnTransfer_Click);
+            // 
+            // cmbODKDrive
+            // 
+            this.cmbODKDrive.Location = new System.Drawing.Point(120, 58);
+            this.cmbODKDrive.Name = "cmbODKDrive";
+            this.cmbODKDrive.Size = new System.Drawing.Size(120, 54);
+            this.cmbODKDrive.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.cmbODKDrive.StateCommon.Border.Rounding = 5;
+            this.cmbODKDrive.TabIndex = 6;
+            // 
+            // cmbTabletID
+            // 
+            this.cmbTabletID.DropDownHeight = 100;
+            this.cmbTabletID.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTabletID.DropDownWidth = 121;
+            this.cmbTabletID.Items.AddRange(new object[] {
+            "Tablet01",
+            "Tablet02",
+            "Tablet03",
+            "Tablet04",
+            "Tablet05",
+            "Tablet06",
+            "Tablet07",
+            "Tablet08",
+            "Tablet09",
+            "Tablet10",
+            "Tablet11",
+            "Tablet12",
+            "Tablet13",
+            "Tablet14",
+            "Tablet15",
+            "Tablet16",
+            "Tablet17",
+            "Tablet18",
+            "Tablet19",
+            "Tablet20",
+            "Tablet21",
+            "Tablet22",
+            "Tablet23",
+            "Tablet24",
+            "Tablet25",
+            "Tablet26",
+            "Tablet27",
+            "Tablet28",
+            "Tablet29",
+            "Tablet30"});
+            this.cmbTabletID.Location = new System.Drawing.Point(119, 27);
+            this.cmbTabletID.Name = "cmbTabletID";
+            this.cmbTabletID.Size = new System.Drawing.Size(121, 25);
+            this.cmbTabletID.StateCommon.ComboBox.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.cmbTabletID.StateCommon.ComboBox.Border.Rounding = 5;
+            this.cmbTabletID.TabIndex = 1;
+            this.cmbTabletID.TabStop = false;
+            // 
+            // tabExport
+            // 
+            this.tabExport.AutoHiddenSlideSize = new System.Drawing.Size(200, 200);
+            this.tabExport.Controls.Add(this.kryptonPanel2);
+            this.tabExport.Flags = 65534;
+            this.tabExport.LastVisibleSet = true;
+            this.tabExport.MinimumSize = new System.Drawing.Size(50, 50);
+            this.tabExport.Name = "tabExport";
+            this.tabExport.Size = new System.Drawing.Size(316, 214);
+            this.tabExport.Text = "Export";
+            this.tabExport.ToolTipTitle = "Page ToolTip";
+            this.tabExport.UniqueName = "31FCF77C5B6A4720AEBC4791A12E81DB";
+            // 
+            // kryptonPanel2
+            // 
+            this.kryptonPanel2.Controls.Add(this.optControl);
+            this.kryptonPanel2.Controls.Add(this.optNonControl);
+            this.kryptonPanel2.Controls.Add(this.btnExportExcel);
+            this.kryptonPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonPanel2.Location = new System.Drawing.Point(0, 0);
+            this.kryptonPanel2.Name = "kryptonPanel2";
+            this.kryptonPanel2.Size = new System.Drawing.Size(316, 214);
+            this.kryptonPanel2.TabIndex = 0;
+            // 
+            // optControl
+            // 
+            this.optControl.Location = new System.Drawing.Point(76, 53);
+            this.optControl.Name = "optControl";
+            this.optControl.Size = new System.Drawing.Size(129, 20);
+            this.optControl.TabIndex = 3;
+            this.optControl.Values.Text = "Export Control data";
+            // 
+            // optNonControl
+            // 
+            this.optNonControl.Checked = true;
+            this.optNonControl.Location = new System.Drawing.Point(75, 79);
+            this.optNonControl.Name = "optNonControl";
+            this.optNonControl.Size = new System.Drawing.Size(155, 20);
+            this.optNonControl.TabIndex = 2;
+            this.optNonControl.Values.Text = "Export Non-control data";
+            // 
+            // btnExportExcel
+            // 
+            this.btnExportExcel.Location = new System.Drawing.Point(112, 138);
+            this.btnExportExcel.Name = "btnExportExcel";
+            this.btnExportExcel.Size = new System.Drawing.Size(93, 29);
+            this.btnExportExcel.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.btnExportExcel.StateCommon.Border.Rounding = 5;
+            this.btnExportExcel.TabIndex = 1;
+            this.btnExportExcel.Values.Text = "Export as XLS";
+            this.btnExportExcel.Click += new System.EventHandler(this.btnExportExcel_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 281);
+            this.Controls.Add(this.kryptonPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Briefcase";
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel)).EndInit();
+            this.kryptonPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonNavigator1)).EndInit();
+            this.kryptonNavigator1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.tabPull)).EndInit();
+            this.tabPull.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).EndInit();
+            this.kryptonPanel1.ResumeLayout(false);
+            this.kryptonPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.cmbTabletID)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabExport)).EndInit();
+            this.tabExport.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel2)).EndInit();
+            this.kryptonPanel2.ResumeLayout(false);
+            this.kryptonPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonPanel kryptonPanel;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton btnDownload;
+        private ComponentFactory.Krypton.Toolkit.KryptonComboBox cmbTabletID;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel1;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel2;
+        private ComponentFactory.Krypton.Toolkit.KryptonListBox cmbODKDrive;
+        private ComponentFactory.Krypton.Navigator.KryptonNavigator kryptonNavigator1;
+        private ComponentFactory.Krypton.Navigator.KryptonPage tabPull;
+        private ComponentFactory.Krypton.Toolkit.KryptonPanel kryptonPanel1;
+        private ComponentFactory.Krypton.Navigator.KryptonPage tabExport;
+        private ComponentFactory.Krypton.Toolkit.KryptonPanel kryptonPanel2;
+        private ComponentFactory.Krypton.Toolkit.KryptonLinkLabel lnkDataDir;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton btnExportExcel;
+        private ComponentFactory.Krypton.Toolkit.KryptonRadioButton optControl;
+        private ComponentFactory.Krypton.Toolkit.KryptonRadioButton optNonControl;
+
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
+using System.IO;
+using System.Diagnostics;
+using System.Xml;
+using ExcelLibrary.SpreadSheet;
+
+namespace Briefcase
+{
+
+    public partial class MainForm : KryptonForm
+    {
+        // Added exporting to XLS
+        //public const string VERSION = "1.20";
+
+        // Added conversion of PSGC to NAME
+        public const string VERSION = "1.21 (10-17-2014)";
+
+
+        protected const string ODK_SETTINGS_FILE = @"\odk\settings\collect.settings";
+        protected string ODK_DESTINATION;
+        protected string ODK_SOURCE;
+
+
+        private DataSet dsPSGC = new DataSet();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+
+
+
+        private void btnExportExcel_Click(object sender, EventArgs e)
+        {
+            ExportXLS();
+        }
+
+
+        private void lnkDataDir_LinkClicked(object sender, EventArgs e)
+        {
+            Process.Start(ODK_DESTINATION);
+        }
+
+
+        private void btnTransfer_Click(object sender, EventArgs e)
+        {
+            Download();
+        }
+
+
+
+
+
+        private void Init()
+        {
+            this.Text = this.Text + " - v" + VERSION;
+            ODK_DESTINATION = Environment.CurrentDirectory + @"\data\download\" ;
+            WatchODK();
+
+            dsPSGC.ReadXml(Environment.CurrentDirectory + @"\data\shared\psgc.xml");
+
+        }
+
+
+        private void Download()
+        {
+            if (cmbODKDrive.SelectedItem == null || cmbTabletID.SelectedItem == null)
+                return;
+
+            string from = cmbODKDrive.SelectedItem + @"\odk";
+            string to = ODK_DESTINATION + cmbTabletID.Text;
+
+            DirectoryInfo fromDir = new DirectoryInfo(from);
+            DirectoryInfo toDir = new DirectoryInfo(to);
+
+            CopyAll(fromDir, toDir);
+
+            MessageBox.Show("Data from " + cmbTabletID.Text + " was downloaded successfully", "Done");
+
+        }
+
+
+        public static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+        {
+            // Check if the target directory exists, if not, create it.
+            if (Directory.Exists(target.FullName) == false)
+            {
+                Directory.CreateDirectory(target.FullName);
+            }
+
+            // Copy each file into it’s new directory.
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
+                fi.CopyTo(Path.Combine(target.ToString(), fi.Name), true);
+            }
+
+            // Copy each subdirectory using recursion.
+            foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+            {
+                DirectoryInfo nextTargetSubDir =
+                    target.CreateSubdirectory(diSourceSubDir.Name);
+                CopyAll(diSourceSubDir, nextTargetSubDir);
+            }
+        }
+
+        private void WatchODK()
+        {
+            Timer t = new Timer();
+            t.Interval = 1000;
+            t.Tick += delegate
+            {
+                FindODKFolders();
+            };
+            t.Start();
+        }
+
+        private void FindODKFolders()
+        {
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                string file = drive.Name + ODK_SETTINGS_FILE;
+                System.Diagnostics.Debug.WriteLine(file + " - " + System.IO.File.Exists(file));
+                if (System.IO.File.Exists(file))
+                {
+                    // add drive
+                    if (cmbODKDrive.Items.IndexOf(drive.Name) == -1)
+                        cmbODKDrive.Items.Add(drive.Name);
+                }
+                else
+                {
+                    if (cmbODKDrive.Items.IndexOf(drive.Name) > -1)
+                        cmbODKDrive.Items.Remove(drive.Name);
+                }
+            }
+        }
+
+        private void ExportXLS()
+        {
+            DataSet ds;
+            if (optControl.Checked)
+                ds = GetControlForm();
+            else
+                ds = GetDefaultForm();
+
+
+            SaveFileDialog dialog = new SaveFileDialog();
+            
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            dialog.FileName = String.Format("export{6}_{0}-{1}-{2}_{3}-{4}-{5}.xls", DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second,(optControl.Checked?"_control":""));
+            dialog.FileOk += delegate
+            {
+                CreateWorkbook(dialog.FileName, ds);
+                FileInfo f = new FileInfo(dialog.FileName);
+                File.Copy(dialog.FileName, Environment.CurrentDirectory + @"\data\export\" + f.Name);
+            };
+            dialog.ShowDialog();
+
+            
+            
+
+            
+        }
+
+        private DataSet GetControlForm()
+        {
+            return XMLtoDS(true);
+        }
+
+        private DataSet GetDefaultForm()
+        {
+            return XMLtoDS(false);
+        }
+
+        private DataSet XMLtoDS(bool control = false)
+        {
+            DataSet ds = new DataSet("DataSet");
+
+            // prepare structure of tables
+            ds.ReadXmlSchema(Environment.CurrentDirectory + @"\data\shared\schema.xsd");
+
+            // get all xml files
+            DirectoryInfo destinationDir = new DirectoryInfo(ODK_DESTINATION);
+
+            DirectoryInfo[] tabletDirs = destinationDir.GetDirectories();
+
+            foreach (DirectoryInfo tabletDir in tabletDirs)
+            {
+                foreach (DirectoryInfo instancesDir in tabletDir.GetDirectories("instances"))
+                {
+                    FileInfo[] xmlFiles = instancesDir.GetFiles(@"uct_form*.xml", SearchOption.AllDirectories);
+                    
+                    foreach (FileInfo f in xmlFiles)
+                    {
+          
+                        if(f.FullName.Contains("uct_form_control") && control)
+                            ds.ReadXml(f.FullName);
+                        if (!f.FullName.Contains("uct_form_control") && !control)
+                            ds.ReadXml(f.FullName);
+                    }
+                }
+
+            }
+            return ds;
+        }
+
+        private void CreateWorkbook(String filePath, DataSet dataset)
+        {
+            if (dataset.Tables.Count == 0)
+                throw new ArgumentException("DataSet needs to have at least one DataTable", "dataset");
+
+            Workbook workbook = new Workbook();
+            foreach (DataTable dt in dataset.Tables)
+            {
+                Worksheet worksheet = new Worksheet(dt.TableName);
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    // Add column header
+                    worksheet.Cells[0, i] = new Cell(dt.Columns[i].ColumnName);
+
+
+
+                    // Populate row data
+                    for (int j = 0; j < dt.Rows.Count; j++)
+                    {
+                        //object value = dt.Rows[j][i];
+                        object value = ProcessData(dt.Columns[i].ColumnName, dt.Rows[j][i]);
+
+
+                        worksheet.Cells[j + 1, i] = new Cell(dt.Rows[j][i] == DBNull.Value ? "" : value);
+                    }
+                }
+                workbook.Worksheets.Add(worksheet);
+            }
+            workbook.Save(filePath);
+        }
+
+
+        private object ProcessData(string columnName, object value)
+        {
+            if (columnName == "Q1.1" || columnName == "Q1.2" || columnName == "Q26" || columnName == "Q29" || columnName == "Q35" || columnName == "Q36.2")
+            {
+                DataRow[] drRows = dsPSGC.Tables[0].Select("PSGC='" + value + "'");
+                if(drRows.Length ==0)
+                    return "Did not find " + value + " in PSGC";
+                return drRows[0]["NAME"];
+            }
+
+            return value;
+        }
+
+    }
+}
